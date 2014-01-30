ea249f73db9b37abb3b6f3577332a0b9095b85a9
arvindayal <arvinddayal@gmail.com>
2014-01-30 12:13:34 -0700

    swapcase functioning

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-var myArr = ("Arvind", "Matt");
+var myArr = "RefactorU";
 
 //reverse a string
-var firstReverse = function(s) {
-	return s.split("").reverse().join("");
+var firstReverse = function(str) {
+	return str.split("").reverse().join("");
 };
+
+//swapcase function
+var swapCase = function(str) {
+	var letter = str.split("");
+	var swapLetter = [];
+	for (var i=0; i<letter.length; i++) {
+	if (letter[i] === letter[i].toUpperCase()) {
+		swapLetter.push( letter[i].toLowerCase());
+	}
+	else if (letter[i] === letter[i].toLowerCase()) {
+		swapLetter.push( letter[i].toUpperCase());
+	}
+	}
+	return swapLetter;
+};
+
